031a30a4a4ab449cb598f5911968b76bd0de3c21
Jantu Deb <jantud54@gmail.com>
2022-05-11 23:05:00 +0530

    added readme with example req

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -3,7 +3,7 @@ const cookieToken = (user) => {
   const options = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    // secure: true,
+    secure: true,
     sameSite: "none",
   };
   return { token, options };
