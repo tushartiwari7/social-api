636a933fc886e765cf7e86e76b5728e2bb2906fc
tushartiwari7 <tushartiwari0172@gmail.com>
2022-05-18 11:26:57 +0530

    cheanged default avatar

--- model/user.model.js
+++ model/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
       secure_url: {
         type: String,
         default:
-          "https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png",
+          "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
       },
     },
     bio: String,
